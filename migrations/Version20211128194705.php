b0fa0f2a2d1539a917216ce185b4b6b970222418
gorskimarcin96 <gorskimarcin96@gmail.com>
2021-11-28 21:47:01 +0100

    Fixed all autoincrement.

---
+++ migrations/Version20211128194705.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211128194705 extends AbstractMigration
+{
+    const TABLES = [
+        'article', 'constant', 'counter', 'cron_job', 'cron_report', 'messenger_messages', 'offer', 'position',
+        'realization', 'slider', 'task', 'user'
+    ];
+
+    public function getDescription(): string
+    {
+        return 'Fixed all autoincrement.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        foreach (self::TABLES as $table) {
+            $this->addSql(sprintf('SELECT setval(\'%s_id_seq\', (SELECT MAX(id) FROM "%s")+1);', $table, $table));
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
