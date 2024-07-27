e5c6cb9a3a486f7d11595af7d2b9eaed6ce8ab0b
Marcin Górski <gorskimarcin96@gmail.com>
2024-07-27 21:10:44 +0200

    tests

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
---
+++ tests/Behat/WebsiteContext.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Behat;
+
+use Behat\Behat\Context\Context;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+final class WebsiteContext implements Context
+{
+    private ?Response $response = null;
+
+    public function __construct(private readonly KernelInterface $kernel)
+    {
+    }
+
+    /**
+     * @When a demo scenario sends a request to :path
+     */
+    public function aDemoScenarioSendsARequestTo(string $path): void
+    {
+        $this->response = $this->kernel->handle(Request::create($path, 'GET'));
+        dd($this->response);
+    }
+
+    /**
+     * @Then the response should be received
+     */
+    public function theResponseShouldBeReceived(): void
+    {
+        if ($this->response === null) {
+            throw new \RuntimeException('No response received');
+        }
+    }
+}
